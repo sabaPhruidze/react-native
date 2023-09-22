c6a46414f634cf11fdbfae6f3b421ae9293aa4b8
Saba Phruidze <sabaphruidze01@gmail.com>
2023-09-22 22:18:51 +0400

    just added a weird input

--- App.js
+++ App.js
@@ -4,15 +4,25 @@ import {
   View,
   AlertButton,
   StatusBar,
+  TextInput,
   Button,
+  SafeAreaView,
 } from "react-native";
 import { useState } from "react";
 
 export default function App() {
   const [count, setCount] = useState(0);
   return (
-    <View style={styles.container}>
-      <View style={{ flex: 3, backgroundColor: "green", width: 350 }}>
+    <SafeAreaView style={styles.container}>
+      <View
+        style={{
+          flex: 3,
+          backgroundColor: "green",
+          width: 350,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
         <Text style={{ fontSize: 50 }}>{count}</Text>
         <StatusBar style="auto" backgroundColor="white" />
         <Button
@@ -21,11 +31,20 @@ export default function App() {
             setCount((current) => current + 1);
           }} // onClick არის
         />
+        <TextInput
+          style={{
+            height: 40,
+            borderColor: "#FFFF00",
+            borderWidth: 1,
+            marginTop: 10,
+          }}
+          placeholder="type here for translate"
+        />
       </View>
       <View style={{ flex: 2, backgroundColor: "yellow", width: 350 }}>
         <Text>text</Text>
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
 
